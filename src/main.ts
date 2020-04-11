366c598ee067b1b97747759cd4bd3ae08770b940
lmtortellis <lucas.tortelli@melhorenvio.com>
2020-04-11 16:19:09 -0300

    feat: add .env file and change port and DB configuration

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from "@nestjs/microservices";
 import { AppModule } from './app.module';
+require('dotenv').config()
 
 async function bootstrap() {
     const app = await NestFactory.createMicroservice(AppModule, {
       transport: Transport.TCP,
       options: {
         host: "127.0.0.1",
-        port: 8081
+        port: process.env.PORT
       }
     });
-    app.listen(() => console.log("Microservice A is listening"));
+    app.listen(() => console.log("Read-API A is listening on port " +process.env.PORT));
 }
 bootstrap();
